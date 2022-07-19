04dd48d8bca62a1867a2414be4dfe8116618ebe2
caducoder <cadudrop@hotmail.com>
2022-07-19 14:18:26 -0300

    Register screen completed

---
+++ src/screens/Details.tsx
@@ -0,0 +1,12 @@
+import { VStack } from "native-base";
+import Header from "../components/Header";
+
+function Details() {
+  return ( 
+    <VStack>
+      <Header title='Solicitação' />
+    </VStack>
+   );
+}
+
+export default Details;
---
+++ src/screens/Register.tsx
@@ -0,0 +1,27 @@
+import { Input, Text, VStack } from "native-base";
+import Button from "../components/Button";
+import Header from "../components/Header";
+
+function Register() {
+  return ( 
+    <VStack flex={1} p={6} bg='gray.600'>
+      <Header title="Nova Solicitação"/>
+
+      <Input 
+        placeholder="Número do patrimônio"
+        mt={5}
+      />
+
+      <Input 
+        placeholder="Descrição do problema"
+        flex={1}
+        mt={5}
+        multiline
+        textAlignVertical="top"
+      />
+      <Button mt={5}>Cadastrar</Button>
+    </VStack>
+   );
+}
+
+export default Register;
